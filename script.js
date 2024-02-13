7932a92dc66ccddbbe7965953da3893d6604cc54
adeoluwa <akanbiadenugba699@gmail.com>
2024-02-14 00:16:32 +0100

    second comit

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
 
 yesBtn.addEventListener("click", () => {
-question.innerHTML = "HAPPY VALENTINE MY LOVE!!!";
-gif.src =
-"https://media2.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+  question.innerHTML = "HAPPY VALENTINE MY LOVE!!!";
+  gif.src = "https://media2.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+  // Hide the yes button
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
 });
 
 noBtn.addEventListener('mouseover', () => {
-const noBtnRect = noBtn.getBoundingClientRect();
-const maxX = window.innerWidth -noBtnRect.width;
-const maxY = window.innerHeight -noBtnRect.height;
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-const randomX = Math.round(Math.random() * maxX);
-const randomY = Math.round(Math.random() * maxY);
+  const randomX = Math.round(Math.random() * maxX);
+  const randomY = Math.round(Math.random() * maxY);
 
-noBtn.style.left = randomX + "px";
-noBtn.style.top = randomY + "px";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
 });
-
